74efb49f312de1193a4b807d6e1c3b3ecb64e82c
Steve Parson <k47swp@mun.ca>
2020-12-08 00:36:58 -0330

    fix: more concise solution

--- Day2Part2.scala
+++ Day2Part2.scala
@@ -8,7 +8,6 @@ object Day2Part2 extends App {
     Source.fromFile("day2input")
       .getLines()
       .map(x => x match { case TupleRegex(n1, n2, l, w) => (n1.toInt, n2.toInt, l, w) })
-      .toArray
       .count(x => { (x._4(x._1 - 1) == x._3(0) && x._4(x._2 - 1) != x._3(0)) ||
                     (x._4(x._1 - 1) != x._3(0) && x._4(x._2 - 1) == x._3(0))     }  )
   )
