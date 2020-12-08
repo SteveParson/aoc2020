f573597bbea0ea84c85db747be58bb79792df047
Steve Parson <k47swp@mun.ca>
2020-12-08 00:37:19 -0330

    refactor: more concise solution

--- Day2Part2.scala
+++ Day2Part2.scala
@@ -7,8 +7,7 @@ object Day2Part2 extends App {
   println(
     Source.fromFile("day2input")
       .getLines()
-      .map(x => x match { case TupleRegex(n1, n2, l, w) => (n1.toInt, n2.toInt, l, w) })
-      .count(x => { (x._4(x._1 - 1) == x._3(0) && x._4(x._2 - 1) != x._3(0)) ||
-                    (x._4(x._1 - 1) != x._3(0) && x._4(x._2 - 1) == x._3(0))     }  )
+      .map(x => x match { case TupleRegex(n1, n2, l, w) => (n1.toInt, n2.toInt, l(0), w) })
+      .count(x => { (x._4(x._1 - 1) == x._3) != (x._4(x._2 -1) == x._3) })
   )
 }
